eb1388d7df381745e58390691f3da1a0aa2e4df1
Jay Lee <jaeho.lee@snu.ac.kr>
2023-07-12 18:53:57 +0900

    :art: Separate simulator.mli

--- lib/simulator.ml
+++ lib/simulator.ml
@@ -1,17 +1,6 @@
 open Base
 
-module React : sig
-  type state = Univ.t
-
-  val render : (unit -> (< render : unit ; .. > as 'a)) -> 'a
-  val useState : state -> state * (state -> unit)
-
-  val useEffect :
-    (unit -> unit) -> (state * (state -> state -> bool)) list -> unit
-
-  val useEffect0 : (unit -> unit) -> unit
-  val reset : unit -> unit
-end = struct
+module React = struct
   type state = Univ.t
 
   let states : (int, state) Hashtbl.t = Hashtbl.create (module Int)
