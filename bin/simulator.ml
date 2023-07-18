3898af152a5221a49a594476fb3cbe000ba036e9
Jay Lee <jaeho.lee@snu.ac.kr>
2023-07-18 11:31:44 +0900

    :construction: Mounting with multiple components

--- bin/simulator.ml
+++ bin/simulator.ml
@@ -4,81 +4,111 @@ open React_trace
 open Simulator.React
 module Uint = Univ.Make_univ (Int)
 module Ustring = Univ.Make_univ (String)
+module Uunit = Univ.Make_univ (Unit)
 
-let component () =
-  let count, setCount = useState (Uint.create 0) in
-  let text, setText = useState (Ustring.create "") in
+module Uint_to_unit = Univ.Make_univ (struct
+  type t = int -> unit
 
-  useEffect0 (fun () -> printf "render\n");
-  useEffect (fun () -> printf "no deps\n") [];
-  useEffect (fun () -> printf "count\n") [ (count, Uint.( = )) ];
-  useEffect (fun () -> printf "text\n") [ (text, Ustring.( = )) ];
-  useEffect
-    (fun () -> printf "all deps\n")
-    [ (count, Uint.( = )); (text, Ustring.( = )) ];
+  let ( = ) _ _ = false
+end)
 
-  object
-    method render =
-      let count, text = (Uint.match_exn count, Ustring.match_exn text) in
-      printf "{ count : %d ; text : \"%s\" }\n" count text
+(* let component () =
+     let count, setCount = useState (Uint.create 0) in
+     let text, setText = useState (Ustring.create "") in
 
-    method click =
-      let count = Uint.match_exn count in
-      setCount (Uint.create (count + 1))
+     useEffect0 (fun () -> printf "render\n");
+     useEffect (fun () -> printf "no deps\n") [];
+     useEffect (fun () -> printf "count\n") [ (count, Uint.( = )) ];
+     useEffect (fun () -> printf "text\n") [ (text, Ustring.( = )) ];
+     useEffect
+       (fun () -> printf "all deps\n")
+       [ (count, Uint.( = )); (text, Ustring.( = )) ];
 
-    method write s =
-      let text = Ustring.match_exn text in
-      setText (Ustring.create (text ^ s))
-  end
+     object
+       method render =
+         let count, text = (Uint.match_exn count, Ustring.match_exn text) in
+         printf "{ count : %d ; text : \"%s\" }\n" count text
 
-let () =
-  printf "=== Deps test ===\n";
-  reset ();
-  let c = render component in
-  c#click;
-  let c = render component in
-  c#click;
-  c#write "Hello, ";
-  let c = render component in
-  c#click;
-  let c = render component in
-  c#write "React!";
-  render component |> ignore
+       method click =
+         let count = Uint.match_exn count in
+         setCount (Uint.create (count + 1))
 
-let great_grandchild () =
-  useEffect0 (fun () -> printf "great-grandchild\n");
-  object
-    method render = ()
-  end
+       method write s =
+         let text = Ustring.match_exn text in
+         setText (Ustring.create (text ^ s))
+     end
 
-let grandchild f =
-  let show, setShow = useState (Uint.create 0) in
-  useEffect0 (fun () ->
-      printf "grandchild\n";
-      f 0;
-      setShow (Uint.create 1));
-  if Uint.match_exn show = 0 then
-    object
-      method render = ()
-    end
-  else great_grandchild ()
+   let () =
+     printf "=== Deps test ===\n";
+     reset ();
+     let c = render component in
+     c#click;
+     let c = render component in
+     c#click;
+     c#write "Hello, ";
+     let c = render component in
+     c#click;
+     let c = render component in
+     c#write "React!";
+     render component |> ignore *)
 
-let child f =
-  useEffect0 (fun () ->
-      printf "child\n";
-      f 1);
-  grandchild f
+let great_grandchild =
+  Composite
+    {
+      name = "GGC";
+      body =
+        (fun _unit ->
+          useEffect0 (fun () -> printf "great-grandchild\n");
+          { component = Null; props = Uunit.create () });
+    }
 
-let parent () =
-  let show, setShow = useState (Uint.create 1) in
-  useEffect0 (fun () -> printf "parent\n");
-  if Uint.match_exn show = 0 then
-    object
-      method render = ()
-    end
-  else child (fun x -> setShow (Uint.create x))
+let grandchild =
+  Composite
+    {
+      name = "GC";
+      body =
+        (fun f ->
+          let show, setShow = useState (Uint.create 0) in
+          useEffect0 (fun () ->
+              printf "grandchild\n";
+              (Uint_to_unit.match_exn f) 0;
+              setShow (Uint.create 1));
+          if Uint.match_exn show = 0 then
+            { component = Null; props = Uunit.create () }
+          else { component = great_grandchild; props = Uunit.create () });
+    }
+
+let child =
+  Composite
+    {
+      name = "C";
+      body =
+        (fun f ->
+          useEffect0 (fun () ->
+              printf "child\n";
+              (Uint_to_unit.match_exn f) 1);
+          { component = grandchild; props = f });
+    }
+
+let parent =
+  Composite
+    {
+      name = "P";
+      body =
+        (fun _unit ->
+          let show, setShow = useState (Uint.create 1) in
+          useEffect0 (fun () -> printf "parent\n");
+          if Uint.match_exn show = 0 then
+            { component = Null; props = Uunit.create () }
+          else
+            {
+              component = child;
+              props = Uint_to_unit.create (fun x -> setShow (Uint.create x));
+            });
+    }
 
 let () =
   reset ();
   printf "=== Chain test [WIP] ===\n";
-  render parent |> ignore
+  let e = render { component = parent; props = Uunit.create () } in
+  render e |> ignore
--- lib/simulator.ml
+++ lib/simulator.ml
@@ -1,50 +1,206 @@
 open Base
+open Stdio
 
 module React = struct
   type state = Univ.t
+  type props = Univ.t
 
-  let states : (int, state) Hashtbl.t = Hashtbl.create (module Int)
-  let effects : (int, state list) Hashtbl.t = Hashtbl.create (module Int)
+  type component =
+    | Null
+    | Composite of { name : string; body : props -> ui_element }
+
+  and ui_element = { component : component; props : props }
+
+  type virtual_dom =
+    | Leaf_node
+    | Tree of {
+        name : string;
+        children : virtual_dom list;
+        states : (int, state) Hashtbl.t;
+        effects : (int, state list) Hashtbl.t;
+      }
+
+  let virtual_dom : virtual_dom option ref = ref None
+  let current_states : (int, state) Hashtbl.t option ref = ref None
+  let current_effects : (int, state list) Hashtbl.t option ref = ref None
   let state_index = ref 0
   let effect_index = ref 0
 
-  let render component =
-    state_index := 0;
-    effect_index := 0;
-    let c = component () in
-    c#render;
-    c
+  let render element =
+    let rec inner { component; props } =
+      let states = Hashtbl.create (module Int) in
+      let effects = Hashtbl.create (module Int) in
+      current_states := Some states;
+      current_effects := Some effects;
+      state_index := 0;
+      effect_index := 0;
+
+      let current_tree =
+        match component with
+        | Null -> Leaf_node
+        | Composite { name; body = f } ->
+            printf "Rendering %s\n" name;
+            let child = f props in
+            Tree
+              {
+                name;
+                children = [ inner child |> snd ];
+                states;
+                effects;
+              }
+      in
+      ({ component; props }, current_tree)
+    in
+    (* Re-render not implemented *)
+    assert (Option.is_none !virtual_dom);
+    let element, tree = inner element in
+    virtual_dom := Some tree;
+    element
 
   let useState (init : state) : state * (state -> unit) =
-    let state =
-      match Hashtbl.find states !state_index with
-      | Some state -> state
-      | None -> init
-    in
-    let index_curr = !state_index in
-    let setState new_val = Hashtbl.set states ~key:index_curr ~data:new_val in
-    Int.incr state_index;
-    (state, setState)
+    match !current_states with
+    | None -> failwith "useState called before render"
+    | Some states ->
+        let state =
+          match Hashtbl.find states !state_index with
+          | Some state -> state
+          | None -> init
+        in
+        let index_curr = !state_index in
+        let setState new_val =
+          Hashtbl.set states ~key:index_curr ~data:new_val
+        in
+        Int.incr state_index;
+        (state, setState)
 
   let useEffect (f : unit -> unit)
       (dependencies : (state * (state -> state -> bool)) list) : unit =
-    let old_deps = Hashtbl.find effects !effect_index in
-    let has_changed =
-      match old_deps with
-      | Some old_deps ->
-          List.existsi dependencies ~f:(fun i (new_state, compare) ->
-              not (compare new_state (List.nth_exn old_deps i)))
-      | None -> true
-    in
-    if has_changed then f ();
-    Hashtbl.set effects ~key:!effect_index ~data:(List.map dependencies ~f:fst);
-    Int.incr effect_index
+    match !current_effects with
+    | None -> failwith "useEffect called before render"
+    | Some effects ->
+        let old_deps = Hashtbl.find effects !effect_index in
+        let has_changed =
+          match old_deps with
+          | Some old_deps ->
+              List.existsi dependencies ~f:(fun i (new_state, compare) ->
+                  not (compare new_state (List.nth_exn old_deps i)))
+          | None -> true
+        in
+        if has_changed then f ();
+        Hashtbl.set effects ~key:!effect_index
+          ~data:(List.map dependencies ~f:fst);
+        Int.incr effect_index
 
   let useEffect0 f = f ()
 
   let reset () =
-    Hashtbl.clear states;
-    Hashtbl.clear effects;
+    virtual_dom := None;
+    current_states := None;
+    current_effects := None;
     state_index := 0;
     effect_index := 0
 end
+
+(* module React = struct
+     type state = Univ.t
+
+     type _ component =
+       | Null : unit component
+       | Composite : string * ('props -> ui_element_wrapper) -> 'props component
+
+     and 'props ui_element = { component : 'props component; props : 'props }
+     and  ui_element_wrapper = < element : 'props. 'props ui_element >
+
+     type virtual_dom =
+       | Leaf_node
+       | Tree of {
+           name : string;
+           children : virtual_dom list;
+           states : (int, state) Hashtbl.t;
+           effects : (int, state list) Hashtbl.t;
+         }
+
+     let virtual_dom : virtual_dom option ref = ref None
+     let current_states : (int, state) Hashtbl.t option ref = ref None
+     let current_effects : (int, state list) Hashtbl.t option ref = ref None
+     let state_index = ref 0
+     let effect_index = ref 0
+
+     let render element =
+       let rec inner :
+           type props. props ui_element -> props ui_element * virtual_dom =
+        fun element ->
+         let { component; props } = element in
+
+         let states = Hashtbl.create (module Int) in
+         let effects = Hashtbl.create (module Int) in
+         current_states := Some states;
+         current_effects := Some effects;
+         state_index := 0;
+         effect_index := 0;
+
+         let current_tree =
+           match component with
+           | Null -> Leaf_node
+           | Composite (name, f) ->
+               let child = f props in
+               Tree { name; children = [ inner child |> snd ]; states; effects }
+         in
+         ({ component; props }, current_tree)
+       in
+       (* Re-render not implemented *)
+       assert (Option.is_none !virtual_dom);
+       let element, tree = inner element in
+       virtual_dom := Some tree;
+       element
+
+     (* let render component =
+       state_index := 0;
+       effect_index := 0;
+       let c = component () in
+       c#render;
+       c *)
+
+     let useState (init : state) : state * (state -> unit) =
+       match !current_states with
+       | None -> failwith "useState called before render"
+       | Some states ->
+           let state =
+             match Hashtbl.find states !state_index with
+             | Some state -> state
+             | None -> init
+           in
+           let index_curr = !state_index in
+           let setState new_val =
+             Hashtbl.set states ~key:index_curr ~data:new_val
+           in
+           Int.incr state_index;
+           (state, setState)
+
+     let useEffect (f : unit -> unit)
+         (dependencies : (state * (state -> state -> bool)) list) : unit =
+       match !current_effects with
+       | None -> failwith "useEffect called before render"
+       | Some effects ->
+           let old_deps = Hashtbl.find effects !effect_index in
+           let has_changed =
+             match old_deps with
+             | Some old_deps ->
+                 List.existsi dependencies ~f:(fun i (new_state, compare) ->
+                     not (compare new_state (List.nth_exn old_deps i)))
+             | None -> true
+           in
+           if has_changed then f ();
+           Hashtbl.set effects ~key:!effect_index
+             ~data:(List.map dependencies ~f:fst);
+           Int.incr effect_index
+
+     let useEffect0 f = f ()
+
+     let reset () =
+       virtual_dom := None;
+       current_states := None;
+       current_effects := None;
+       state_index := 0;
+       effect_index := 0
+   end *)
