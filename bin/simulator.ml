e4c4c0d64728eaeb49badc07ed7a6174a5961706
Jay Lee <jaeho.lee@snu.ac.kr>
2023-07-31 23:44:34 +0900

    :sparkles: Fully working prototype of React simulator

--- bin/simulator.ml
+++ bin/simulator.ml
@@ -53,71 +53,54 @@ end)
      render component |> ignore *)
 
 let great_grandchild =
-  Composite
-    {
-      name = "GGC";
-      body =
-        (fun _unit ->
-          useEffect (fun () -> printf "great-grandchild\n") ();
-          [ { component = Null; props = Uunit.create () } ]);
-    }
+  {
+    name = "GGC";
+    body =
+      (fun _unit ->
+        useEffect (fun () -> printf "great-grandchild\n") ();
+        []);
+  }
 
 let grandchild =
-  Composite
-    {
-      name = "GC";
-      body =
-        (fun f ->
-          let show, setShow = useState (Uint.create 0) in
-          useEffect
-            (fun () ->
-              printf "grandchild\n";
-              (Uint_to_unit.match_exn f) 0;
-              setShow (Uint.create 1))
-            ();
-          if Uint.match_exn show = 0 then
-            [ { component = Null; props = Uunit.create () } ]
-          else [ { component = great_grandchild; props = Uunit.create () } ]);
-    }
+  {
+    name = "GC";
+    body =
+      (fun f ->
+        let show, setShow = useState (module Uint) 0 in
+        useEffect
+          (fun () ->
+            printf "grandchild\n";
+            (Uint_to_unit.match_exn f) 0;
+            setShow 1)
+          ();
+        if show = 0 then []
+        else [ { component = great_grandchild; props = Uunit.create () } ]);
+  }
 
 let child =
-  Composite
-    {
-      name = "C";
-      body =
-        (fun f ->
-          useEffect
-            (fun () ->
-              printf "child\n";
-              (Uint_to_unit.match_exn f) 1)
-            ();
-          [ { component = grandchild; props = f } ]);
-    }
+  {
+    name = "C";
+    body =
+      (fun f ->
+        useEffect
+          (fun () ->
+            printf "child\n";
+            (Uint_to_unit.match_exn f) 1)
+          ();
+        [ { component = grandchild; props = f } ]);
+  }
 
 let parent =
-  Composite
-    {
-      name = "P";
-      body =
-        (fun _unit ->
-          let show, setShow = useState (Uint.create 1) in
-          useEffect (fun () -> printf "parent\n") ();
-          if Uint.match_exn show = 0 then
-            [ { component = Null; props = Uunit.create () } ]
-          else
-            [
-              {
-                component = child;
-                props = Uint_to_unit.create (fun x -> setShow (Uint.create x));
-              };
-            ]);
-    }
+  {
+    name = "P";
+    body =
+      (fun _unit ->
+        let show, setShow = useState (module Uint) 1 in
+        useEffect (fun () -> printf "parent\n") ();
+        if show = 0 then []
+        else [ { component = child; props = Uint_to_unit.create setShow } ]);
+  }
 
 let () =
-  reset ();
   printf "=== Chain test ===\n";
-  printf "[Pass 1]\n";
-  let e = render { component = parent; props = Uunit.create () } in
-  printf "[Pass 2]\n";
-  let _e = render e in
-  ()
+  render { component = parent; props = Uunit.create () } |> ignore
